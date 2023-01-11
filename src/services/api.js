5f8d9b7dde262a702170b1cd948a044561d3bbda
Harrison <Harrison.couto@outlook.com>
2023-01-11 16:08:11 -0300

    requisito 01 group 21

--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,11 @@ export async function getCategories() {
   return response;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+  const data = await fetch(url);
+  const response = await data.json();
+  return response;
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
 }
 
