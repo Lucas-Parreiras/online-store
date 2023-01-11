2c4b797b78e685850b226d76d61e7d8bd1de5130
Marcos Adriano <marcosadriano.ti@gmail.com>
2023-01-11 16:49:45 -0300

    Group 21 - Requisito 02 - Mensagem de lista de produtos vazia e Input de pesquisa

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import SearchInput from "../components/SearchInput";
 
 class MainPage extends React.Component{
   render(){
     return(
-
+      <>
+        <SearchInput />
+        <p data-testid="home-initial-message">Digite algum termo de pesquisa ou escolha uma categoria.</p>
+      </>
     )
   }
 }
