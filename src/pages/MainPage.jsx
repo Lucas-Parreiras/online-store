bbb3ec3954a2a3bbf7b4070c439328f612a1323e
Lucas Parreiras <lucassouzaparreiras@hotmail.com>
2023-01-11 18:01:54 -0300

    Grupo 21 - Requisito 03 - Página de carrinho de compras e botão de redirecionamento

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import SearchInput from '../components/SearchInput';
+import navegation from '../services/navegation';
 
 class MainPage extends React.Component {
   render() {
+    const { history } = this.props;
     return (
       <>
         <SearchInput />
+        <button
+          data-testid="shopping-cart-button"
+          type="submit"
+          onClick={ () => navegation('/shoppingcart', history) }
+        >
+          Carrinho
+        </button>
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
@@ -13,5 +23,9 @@ class MainPage extends React.Component {
     );
   }
 }
+
+MainPage.propTypes = {
+  history: PropTypes.shape({ push: PropTypes.func }).isRequired,
+};
 
 export default MainPage;
