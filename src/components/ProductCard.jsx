7b34558313b5ad6174132fb673d79a4f136d3b78
Marcos Adriano <marcosadriano.ti@gmail.com>
2023-01-12 18:43:17 -0300

    Group 21 - Requisito 08 - Guarda produtos no cart

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,17 +3,35 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 export default class ProductCard extends Component {
+  saveProduct = (objProduct) => {
+    const actualCart = JSON.parse(localStorage.getItem('cart'));
+    if (actualCart === null) {
+      localStorage.setItem('cart', JSON.stringify([objProduct]));
+    } else {
+      localStorage.setItem('cart', JSON.stringify([objProduct, ...actualCart]));
+    }
+  };
+
   render() {
     const { image, price, title, id } = this.props;
     const link = `/product/${id}`;
     return (
-      <Link to={ link } data-testid="product-detail-link">
-        <div data-testid="product">
-          <h2>{title}</h2>
-          <img src={ image } alt="" />
-          <p>{price}</p>
-        </div>
-      </Link>
+      <>
+        <Link to={ link } data-testid="product-detail-link">
+          <div data-testid="product">
+            <h2>{title}</h2>
+            <img src={ image } alt="" />
+            <p>{price}</p>
+          </div>
+        </Link>
+        <button
+          type="submit"
+          data-testid="product-add-to-cart"
+          onClick={ () => this.saveProduct({ id, title, price, image }) }
+        >
+          Adicionar ao carrinho
+        </button>
+      </>
     );
   }
 }
